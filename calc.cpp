aa94e97b423d2680bc2444dddcf6dc196d77c04a
Prakhar Agarwal <prakhar4444yash@gmail.com>
2019-01-16 17:37:12 -1200

    Adde the main section in the program

--- calc.cpp
+++ calc.cpp
@@ -12,5 +12,28 @@ class calculate
 };
 int main()
 {
+	int n
 	calculate c;
+	cout<<"Enter two numbers on which you want to perform arithmetic operations";
+	cin>>a>>b;
+	cout<"Enter 1 for addition\nEnter 2 for subtraction\nEnter 3 for multiplication\nEnter 4 for division";
+	cin>>n;
+	switch(n)
+	{
+		case 1:
+			c.add(a,b);
+			break;
+		case 2: 
+			c.sub(a,b);
+			break;
+		case 3:
+			c.mul(a,b);
+			break;
+		case 4:
+			c.div(a,b);
+			break;
+		default:
+			exit(0);
+	}
+
 }
