d19895fb2a8a6a2ae284c5c4a6eb5d14a194852f
Prakhar Agarwal <prakhar4444yash@gmail.com>
2019-01-16 17:26:20 -1200

    Programm to perform arithmetic operations

---
+++ calc.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+class calculate
+{
+	private:
+		int x,y;
+
+	public:
+		int add(int x,int y);
+		int sub(int x,int y);
+		int mul(int x,int y);
+		int div(int x,int y);
+};
+int main()
+{
+	calculate c;
+}
